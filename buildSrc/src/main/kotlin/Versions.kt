013391e918cb285cd1755fb9025466b0ee500046
Julio Marcondes <jccmarcondes@gmail.com>
2022-06-22 10:00:11 -0300

    Updated gradle version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -13,7 +13,7 @@ object Versions {
     val bugsnag = "3.6.3"
 
     val junitGradle = "1.2.0"
-    val androidGradleVersion = "4.2.0"
+    val androidGradleVersion = "7.1.3"
     val gradlePluginPublish = "0.20.0"
     val gradlePluginShadow = "7.1.2"
 
